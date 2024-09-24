fe85a6b4f922fe8b12c08345d26fabce9589c366
RainOfStone <bloxooff@gmail.com>
2024-09-23 20:18:57 -0400

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(a, b) {
+  let a = 'hey', b = 0
+do {
+  b++
+  if (b < 3){
+  a = a + a
+  }
+} while (b < 3)
+return a
+}
 // Do not edit below this line
 module.exports = repeatString;
